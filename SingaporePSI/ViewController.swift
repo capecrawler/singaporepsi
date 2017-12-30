3d9124b64b07347d95c6ac3ec965291a98dec49e
mariocape <mariocape1106@gmail.com>
2017-12-30 18:43:43 +0800

    chore: setup project

---
+++ SingaporePSI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingaporePSI
+//
+//  Created by Mario Antonio Cape on 30/12/2017.
+//  Copyright © 2017 Mario Antonio Cape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
