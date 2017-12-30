1a6348baf39b43f74a6d9803a0c8ba7d531ba724
mariocape <mariocape1106@gmail.com>
2017-12-30 19:38:39 +0800

    chore: add config file for api keys and other configuration data

--- SingaporePSI/AppDelegate.swift
+++ SingaporePSI/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyCWIpr5uavwUGGlYNfC8JBvH5KdMCC6LDA")
+        GMSServices.provideAPIKey(Config.gmapsAPIKey)
         
         return true
     }
---
+++ SingaporePSI/Config/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  SingaporePSI
+//
+//  Created by Mario Antonio Cape on 30/12/2017.
+//  Copyright © 2017 Mario Antonio Cape. All rights reserved.
+//
+
+import Foundation
+
+
+struct Config {
+    static let gmapsAPIKey = "AIzaSyCWIpr5uavwUGGlYNfC8JBvH5KdMCC6LDA"      
+}
