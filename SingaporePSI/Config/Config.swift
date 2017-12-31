a9275fc555197516316482ab607a50eca93f253b
mariocape <mariocape1106@gmail.com>
2017-12-31 10:19:18 +0800

    chore: add serverconfig

--- SingaporePSI/Config/Config.swift
+++ SingaporePSI/Config/Config.swift
@@ -8,7 +8,14 @@
 
 import Foundation
 
-
 struct Config {
-    static let gmapsAPIKey = "AIzaSyCWIpr5uavwUGGlYNfC8JBvH5KdMCC6LDA"      
+    static let gmapsAPIKey = "AIzaSyCWIpr5uavwUGGlYNfC8JBvH5KdMCC6LDA"
+    static let serverAPIKey = "A785Y5RosQxFC1T1cG1JoYPhkmkbcDlep"
+    static let baseURL = "https://api.data.gov.sg/v1"
+}
+
+struct ServerConfig {
+    var baseURL: String
+    var headers: [String: String]
+    static let appConfig = ServerConfig(baseURL: Config.baseURL, headers: ["api-key": Config.serverAPIKey])
 }
